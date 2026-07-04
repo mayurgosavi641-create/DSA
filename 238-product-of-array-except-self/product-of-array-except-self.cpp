8c203512ae68d34994f9babd1212bfe856b3c3d5
mayurgosavi641-create <mayurgosavi641@gmail.com>
2026-07-05 00:13:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (60.82%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -3,20 +3,18 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
     int n=nums.size();
     vector<int>ans(n,1);
-    vector<int>leftproduct(n,1);
-        vector<int>rightproduct(n,1);
+        
     //left product 
     for(int i=1;i<n;i++){
-        leftproduct[i]=leftproduct[i-1]*nums[i-1];
+        ans[i]=ans[i-1]*nums[i-1];
     }
     //right product
+    int suffix=1;
     for(int i=n-2;i>=0;i--){
-        rightproduct[i]=rightproduct[i+1]*nums[i+1];
+        suffix = suffix*nums[i+1];
+        ans[i]=ans[i]*suffix;
     }
-    // final ans
-    for(int i=0;i<n;i++){
-        ans[i]=leftproduct[i]*rightproduct[i];
-    }
+ 
     return ans;
 
     }
