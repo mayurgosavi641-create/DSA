4005f5c6bc0369b144f04162b3dc4de62b4179d6
Mayur Gosavi <mayurgosavi641@gmail.com>
2026-08-21 23:25:12 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (43.53%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -3,28 +3,27 @@ public:
     int searchInsert(vector<int>& nums, int target) {
       int start=0;
       int end=nums.size()-1;
-       if(target<=nums[start]){
+      if(target<=nums[start]){
         return start;
-       }
-        if(target>nums[end]){
-            return end+1;
-        }
-      while(start<=end){
+      }
+      if(target>nums[end]){
+        return end+1;
+      }
+    while(start<=end){
         int mid=(start+end)/2;
-        if(target==nums[mid]){
+        if(nums[mid]==target){
             return mid;
         }
-       
-        if(target>=nums[mid] && target<=nums[mid+1]){
-            return mid+1;
+        else if(nums[mid]>target && nums[mid-1]<target){
+            return mid;
         }
-        else if(target>nums[mid]){
+        else if(nums[mid]<target){
             start=mid+1;
         }
-        else{
+        else {
             end=mid-1;
         }
-      }
-      return -1;
+    }
+    return -1;
     }
 };
