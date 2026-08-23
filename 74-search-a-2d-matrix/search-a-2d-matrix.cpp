40b04c8151f15e6ec721d4bfa692a529b2d1f963
Mayur Gosavi <mayurgosavi641@gmail.com>
2026-08-23 19:34:59 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (79.15%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-      int m=matrix.size();
-      int n=matrix[0].size();
+      int n=matrix.size();
+      int m=matrix[0].size();
       int startrow=0;
-      int endrow=m-1;
+      int endrow=n-1;
       while(startrow<=endrow){
         int midrow=(startrow+endrow)/2;
-        if(target>=matrix[midrow][0] && target<=matrix[midrow][n-1]){
+        if(matrix[midrow][0]<=target && matrix[midrow][m-1]>=target){
             int start=0;
-            int end=n-1;
+            int end=m-1;
             while(start<=end){
                 int mid=(start+end)/2;
                 if(matrix[midrow][mid]==target){
                     return true;
-                }
-                else if(matrix[midrow][mid]<target){
-                    start=mid+1;
 
                 }
-                else{
+                else if(matrix[midrow][mid]>target){
                     end=mid-1;
+                }
+                else{
+                    start=mid+1;
                 }
             }
             return false;
         }
-        else if(target<=matrix[midrow][0]){
+        else if(matrix[midrow][0]>target){
             endrow=midrow-1;
         }
         else {
             startrow=midrow+1;
         }
       } 
-      return false; 
+      return false;
     }
 };
