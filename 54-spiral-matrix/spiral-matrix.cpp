df06c5db042433bc869427ecf03b99bdc22c88a2
Mayur Gosavi <mayurgosavi641@gmail.com>
2026-08-27 19:22:24 +0530

    Time: 2 ms (1.84%) | Memory: 9.5 MB (18.39%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -2,37 +2,37 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int>ans;
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int startrow=0;
-        int endrow=m-1;
-        int startcolmn=0;
-        int endcolmn=n-1;
-        while(startrow<=endrow && startcolmn<=endcolmn){
-            for(int i=startcolmn;i<=endcolmn;i++){
-                ans.push_back(matrix[startrow][i]);
-            }
-            for(int i=startrow+1;i<=endrow;i++){
-
-                ans.push_back(matrix[i][endcolmn]);
-            }
-            for(int i=endcolmn-1;i>=startcolmn;i--){
-                if(startrow==endrow){
-                    break;
-                }
-                ans.push_back(matrix[endrow][i]);
-            }
-            for(int i=endrow-1;i>startrow;i--){
-                if(startcolmn==endcolmn){
-                    break;
-                }
-                ans.push_back(matrix[i][startcolmn]);
-            }
-            startrow++;
-            endrow--;
-            startcolmn++;
-            endcolmn--;
+       int m=matrix.size();
+       int n=matrix[0].size();
+       int startrow=0;
+       int endrow=m-1;
+       int startcolumn=0;
+       int endcolumn=n-1;
+       while(startrow<=endrow && startcolumn<=endcolumn){
+        for(int i=startrow;i<=endcolumn;i++){
+            ans.push_back(matrix[startrow][i]);
         }
-        return ans;
+        for(int i=startrow+1;i<=endrow;i++){
+
+            ans.push_back(matrix[i][endcolumn]);
+        }
+        for(int i=endcolumn-1;i>=startcolumn;i--){
+            if(startrow==endrow){
+                break;
+            }
+            ans.push_back(matrix[endrow][i]);
+        }
+        for(int i=endrow-1;i>startrow;i--){
+            if(startcolumn==endcolumn){
+                break;
+            }
+            ans.push_back(matrix[i][startcolumn]);
+        }
+        startrow++;
+        endrow--;
+        startcolumn++;
+        endcolumn--;
+       }
+       return ans;
     }
 };
