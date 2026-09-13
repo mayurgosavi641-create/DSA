d14b6e53c4b4d14c6d0a6950e599435bc6bd65f4
Mayur Gosavi <mayurgosavi641@gmail.com>
2026-09-13 19:26:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (76.99%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
  bool isAlphaNumeric(char ch){
-        if((ch>='0' && ch<='9') || (tolower(ch)>='a' && tolower(ch)<='z')){
-            return true;
-        }
-        return false;
+   if((ch>='0' && ch<='9') || (tolower(ch)>='a' && tolower(ch)<='z')){
+    return true;
+   }
+   return false;
  }
     bool isPalindrome(string s) {
-       int start=0;
-       int end=s.length()-1;
-       while(start<end){
+     int start=0;
+     int end=s.length()-1;
+     while(start<end){
         if(! isAlphaNumeric(s[start])){
             start++;
             continue;
@@ -21,8 +21,10 @@ public:
         if(tolower(s[start]) != tolower(s[end])){
             return false;
         }
-        start++,end--;
-       }
-       return true;
+        start++;
+        end--;
+
+     }
+     return true;
     }
 };
