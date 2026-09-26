be3f7a953268db6752dad61e78fc781bac4cb024
Mayur Gosavi <mayurgosavi641@gmail.com>
2026-09-26 20:25:54 +0530

    Time: 23 ms (54.86%) | Memory: 17.5 MB (58.45%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-      vector<vector<int>>ans;
-      sort(nums.begin(),nums.end());
-      for(int i=0;i<nums.size();i++){
+     vector<vector<int>>ans;
+     sort(nums.begin(),nums.end());
+     for(int i=0;i<nums.size();i++){
         if(i>0 && nums[i]==nums[i-1]){
             continue;
         }
@@ -14,7 +14,7 @@ public:
             int k=j+1;
             int l=nums.size()-1;
             while(k<l){
-                long long sum=(long long)nums[i]+(long long)nums[j]+(long long)nums[k]+(long long)nums[l];
+                long long sum=(long long) nums[i]+(long long)nums[j]+(long long)nums[k]+(long long)nums[l];
                 if(sum<target){
                     k++;
                 }
@@ -22,16 +22,16 @@ public:
                     l--;
                 }
                 else{
-                ans.push_back({nums[i],nums[j],nums[k],nums[l]});
-                k++;
-                l--;
-                while(k<l && nums[k]==nums[k-1]){
+                    ans.push_back({nums[i],nums[j],nums[k],nums[l]});
                     k++;
+                    l--;
+                    while(k<l && nums[k]==nums[k-1]){
+                        k++;
+                    }
                 }
-            }  
             }
         }
-      }
-      return ans;  
+     }
+     return ans;
     }
 };
